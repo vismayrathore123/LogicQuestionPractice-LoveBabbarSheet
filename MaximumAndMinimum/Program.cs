2118fba3a415b7f820baa3a31683b38733591ff6
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-04 15:20:29 +0530

    added reverse and max&min

---
+++ MaximumAndMinimum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumAndMinimum
+{
+    internal class Program
+    {
+        public static Tuple<long,long> getMinMax(int[] arr)
+        {
+            int min = arr[0];
+            int max = arr[0];
+            
+            foreach(int num in arr)
+            {
+                if (num < min)
+                {
+                    min= num;
+                }
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return Tuple.Create((long)min,(long)max);
+
+        }
+        static void Main(string[] args)
+        {
+            int [] arr =  { 3, 2, 1, 56 };
+            var result = getMinMax(arr);
+            Console.WriteLine("Minimum" + result.Item1 + ", Maximum" + result.Item2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ReverseTheArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Array.Reverse(arr);
+            Console.WriteLine(string.Join(",", arr));
+            Console.ReadLine();
+            
+        }
+    }
+}
