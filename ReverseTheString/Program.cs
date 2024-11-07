aaaf3e53701aecdcb38fb453fb0ae5c18bb53569
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-07 18:32:51 +0530

    added string questions

---
+++ ReverseTheString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseTheString
+{
+    internal class Program
+    {
+        public static string ReverseTheString(string str)
+        {
+            string str1 = ""; 
+
+         
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                str1 += str[i]; 
+            }
+
+            return str1;
+        }
+
+        static void Main(string[] args)
+        {
+            string str = "Evision";
+            string result = Program.ReverseTheString(str); 
+            Console.WriteLine("Reversed string is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RotateArrayByOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateArrayByOne
+{
+    internal class Program
+    {
+        public static void rotateArray(int[] a)
+        {
+            int n=a.Length;
+            int last = a[n-1];
+            for(int i=4;i>0;i--)
+            {
+                a[i] = a[i - 1];
+            }
+            a[0] = last;
+            Console.WriteLine("Rotated array is:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+             Program.rotateArray(a);
+            
+
+        }
+    }
+}
