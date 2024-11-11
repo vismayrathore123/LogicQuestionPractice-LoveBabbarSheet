c9599bf2a3cd7f15610f69c717b0225c5728c6f9
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-11 15:32:55 +0530

    few more on stack

---
+++ CheckIfAgivenStackIsempty/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CheckIfAgivenStackIsempty")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("CheckIfAgivenStackIsempty")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ed27d99-9f7b-4e4c-abdd-6a4810679adb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Push5integersontoastackandthenpopthemonebyone/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Push 5 integers onto a stack and then pop them one by one.
+namespace Push5integersontoastackandthenpopthemonebyone
+{
+    internal class Program
+    {
+        static void push(Stack<int> stack)
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);  
+            stack.Push(4);
+            stack.Push(5);
+
+
+            Console.WriteLine("push 1,2,3,4,5");
+        }
+        static void popAll(Stack<int> stack)
+        {
+            while (stack.Count > 0)
+            {
+                int poppedElement = stack.Pop();
+                Console.WriteLine("Popped element: " + poppedElement);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            push(stack);
+            popAll(stack); 
+            Console.ReadLine();
+        }
+    }
+}
