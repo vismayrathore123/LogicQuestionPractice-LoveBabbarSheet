e645bcb8d6cb26385a49ae444d6d949430000bed
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-13 15:48:10 +0530

    added arrraysubset& checkequal

---
+++ ArraySubset/Program.cs
@@ -0,0 +1,52 @@
+﻿
+//Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m, where both arrays may contain duplicate elements. The task is to determine whether array a2 is a subset of array a1. It's important to note that both arrays can be sorted or unsorted. Additionally, each occurrence of a duplicate element within an array is considered as a separate element of the set.
+//Example 1:
+//Input:
+//a1[] = {11, 7, 1, 13, 21, 3, 7, 3}
+//a2[] = {11, 3, 7, 1, 7}
+//Output:
+//Yes
+//Explanation:
+//a2[] is a subset of a1[]Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m, where both arrays may contain duplicate elements. The task is to determine whether array a2 is a subset of array a1. It's important to note that both arrays can be sorted or unsorted. Additionally, each occurrence of a duplicate element within an array is considered as a separate element of the set.
+//Example 1:
+//Input:
+//a1[] = {11, 7, 1, 13, 21, 3, 7, 3}
+//a2[] = {11, 3, 7, 1, 7}
+//Output:
+//Yes
+//Explanation:
+//a2[] is a subset of a1[]
+
+using System;
+using System.Collections.Generic;
+
+namespace ArraySubset
+{
+    internal class Program
+    {
+        static string arraySubset(int[]a1, int[] a2)
+        {
+            List<int> list=new List<int>(a1);
+            foreach(int element in a2)
+            {
+                if (list.Contains(element))
+                {
+                    list.Remove(element);
+                }
+                else
+                {
+                    return "No";
+                }
+            }
+            return "yes";
+        }
+        static void Main(string[] args)
+        {
+            int []a1 = { 11, 7, 1, 13, 21, 3, 7, 3 };
+            int []a2 = { 11, 3, 7, 1, 7 };
+            string  result=Program.arraySubset(a1, a2); 
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CheckEqualArray/Program.cs
@@ -0,0 +1,59 @@
+﻿//Given two arrays arr1 and arr2 of equal size, the task is to find whether the given arrays are equal. Two arrays are said to be equal if both contain the same set of elements, arrangements (or permutations) of elements may be different though.
+//Note: If there are repetitions, then counts of repeated elements must also be the same for two arrays to be equal.
+//Examples:
+//Input: arr1[] = [1, 2, 5, 4, 0], arr2[] = [2, 4, 5, 0, 1]
+//Output: true
+//Explanation: Both the array can be rearranged to [0,1,2,4,5]
+//Input: arr1[] = [1, 2, 5], arr2[] = [2, 4, 15]
+//Output: false
+//Explanation: arr1[] and arr2[] have only one common value.Given two arrays arr1 and arr2 of equal size, the task is to find whether the given arrays are equal. Two arrays are said to be equal if both contain the same set of elements, arrangements (or permutations) of elements may be different though.
+//Note: If there are repetitions, then counts of repeated elements must also be the same for two arrays to be equal.
+//Examples:
+//Input: arr1[] = [1, 2, 5, 4, 0], arr2[] = [2, 4, 5, 0, 1]
+//Output: true
+//Explanation: Both the array can be rearranged to [0,1,2,4,5]
+//Input: arr1[] = [1, 2, 5], arr2[] = [2, 4, 15]
+//Output: false
+//Explanation: arr1[] and arr2[] have only one common value.
+
+using System;
+
+namespace CheckEqualArray
+{
+    internal class Program
+    {
+        static bool isEqual(int[]arr, int [] arr1)
+        {
+            if (arr.Length != arr1.Length)
+            {
+                return false;
+            }
+            Array.Sort(arr);
+            Array.Sort(arr1);
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != arr1[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 5, 4 };
+            int[] arr1 = { 3, 3, 9, 9, 7 };
+            bool result = Program.isEqual(arr, arr1);
+
+
+            int[] arr3 = { 1, 2, 5, 4, 0 };
+            int[] arr4 = { 2, 4, 5, 0, 1 };
+
+            result = Program.isEqual(arr3, arr4);
+            Console.WriteLine(result); 
+            
+            Console.ReadLine();
+
+        }
+    }
+}
