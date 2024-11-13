4e047f63a4adb647e71d4b9123796079ec859753
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-13 14:23:36 +0530

    repush extra space solution

---
+++ ReverseStringWithoutExtraSpace/Program.cs
@@ -0,0 +1,33 @@
+﻿
+//Reverse the String Without Using Additional Space
+// - Question: Reverse a string in place (modifying the original string) without using extra space.
+// -Example: Input: "hello", Output: "olleh"
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseStringWithoutExtraSpace
+{
+    internal class Program
+    {
+        public static string reverseStringWithoutExtraSpace(string str)
+        {
+            string[] words=str.Split(' ');
+            for(int i = 0; i < words.Length; i++)
+            {
+                char[] charArray=words[i].ToCharArray();
+                Arra
+            }
+        }
+        static void Main(string[] args)
+        {
+            string str = "hello world";
+            string result = Program.reverseStringWithoutExtraSpace(str);
+            Console.WriteLine(result);  
+            Console.ReadLine();
+        }
+    }
+}
--- ReverseTheString/Program.cs
+++ ReverseTheString/Program.cs
@@ -6,21 +6,31 @@ namespace ReverseTheString
     {
         public static string ReverseTheString(string str)
         {
-            string str1 = ""; 
+            // Convert the string to a character array since strings are immutable in C#
+            char[] charArray = str.ToCharArray();
 
-         
-            for (int i = str.Length - 1; i >= 0; i--)
+            int left = 0;
+            int right = charArray.Length - 1;
+
+            // Swap the characters from both ends towards the center
+            while (left < right)
             {
-                str1 += str[i]; 
+                char temp = charArray[left];
+                charArray[left] = charArray[right];
+                charArray[right] = temp;
+
+                left++;
+                right--;
             }
 
-            return str1;
+            // Return the new reversed string
+            return new string(charArray);
         }
 
         static void Main(string[] args)
         {
             string str = "The quick brown fox";
-            string result = Program.ReverseTheString(str); 
+            string result = Program.ReverseTheString(str);
             Console.WriteLine("Reversed string is: " + result);
             Console.ReadLine();
         }
