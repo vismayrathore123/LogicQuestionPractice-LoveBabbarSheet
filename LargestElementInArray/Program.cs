97904b402bd3f14e958464a47792f2b2fb5faee5
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-13 16:04:29 +0530

    added largest eleent in array

---
+++ LargestElementInArray/Program.cs
@@ -0,0 +1,54 @@
+﻿//Given an array arr[]. The task is to find the largest element and return it.
+//Examples:
+//Input: arr = [1, 8, 7, 56, 90]
+//Output: 90
+//Explanation: The largest element of the given array is 90.
+//Input: arr = [5, 5, 5, 5]
+//Output: 5
+//Explanation: The largest element of the given array is 5.
+//Input: arr = [10]
+//Output: 10
+//Explanation: There is only one element which is the largest.Given an array arr[]. The task is to find the largest element and return it.
+//Examples:
+//Input: arr = [1, 8, 7, 56, 90]
+//Output: 90
+//Explanation: The largest element of the given array is 90.
+//Input: arr = [5, 5, 5, 5]
+//Output: 5
+//Explanation: The largest element of the given array is 5.
+//Input: arr = [10]
+//Output: 10
+//Explanation: There is only one element which is the largest.
+
+using System;
+
+namespace LargestElementInArray
+{
+    internal class Program
+    {
+        static int LargestElement(int[] arr)
+        {
+            
+            int max = arr[0];
+
+            
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i]; 
+                }
+            }
+
+            return max; 
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 8, 7, 56, 90 };
+            int result = Program.LargestElement(arr);
+            Console.WriteLine("Largest element: " + result);
+            Console.ReadLine();
+        }
+    }
+}
