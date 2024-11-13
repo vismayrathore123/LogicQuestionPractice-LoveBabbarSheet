012e1faa5b3ef8174c1bfafd0053c01a4483cb10
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-13 13:42:35 +0530

    not completed reverse string with special character

---
+++ ReverseWordsWithSpecialCharacter/Program.cs
@@ -0,0 +1,38 @@
+﻿
+//Reverse Words with Special Characters
+// - Question: Reverse only the words in a string while keeping special characters in their original
+//positions.
+// - Example: Input:"he!llo wo@rld", Output: "ol!leh dl@row"
+
+//NOT COMPLETED YET
+
+using System;
+using System.Linq;
+
+namespace ReverseWordsWithSpecialCharacter
+{
+    internal class Program
+    {
+        public static string reverseWordsWithSpecialCharacter(string str)
+        {
+            string[] words=str.Split(' ');
+            for(int i=0;i<words.Length; i++)
+            {
+                char[] charArray=words[i].ToCharArray();
+                Array.Reverse(charArray);
+                words[i]=new string(charArray);
+            }
+            string output=string.Join(" ", words);
+            return output;
+        }
+            
+        static void Main(string[] args)
+        {
+            string str= "he!llo wo@rld";
+            string result = Program.reverseWordsWithSpecialCharacter(str);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
