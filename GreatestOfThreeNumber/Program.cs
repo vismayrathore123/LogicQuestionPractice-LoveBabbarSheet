eccbbb977b2d0cbb04d781ebdb8df3eb6822a645
vismay rathore <vismayrathore2021@gmail.com>
2024-12-04 14:11:54 +0530

    greatest of three number

---
+++ GreatestOfThreeNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestOfThreeNumber
+{
+    internal class Program
+    {
+        static int greatestOfThreeNumber(int a,int b,int c)
+        {
+            if(a>=b && a >= c)
+            {
+                Console.WriteLine("a is greater number");
+                return a;
+            }
+            else if(b>=a && b >= c)
+            {
+                Console.WriteLine("b is greater number");
+                return b;
+            }
+            else {
+                Console.WriteLine("C is greater number");
+                return c;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int a = 10,b=20,c=30;
+            int result = Program.greatestOfThreeNumber(a, b, c);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
