2d0d8323883342db4ff9d98e034fa43758922530
Aayush verma <Aayush.verma@emorphis.in>
2024-12-25 12:38:10 +0530

    factorial number

---
+++ FactorialNumber/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace FactorialNumber
+{
+    internal class Program
+    {
+        static int factorialNumber(int n)
+        {
+            int res = 1; 
+
+            if (n == 0) return 1; 
+            for (int i = 2; i <= n; i++) 
+            {
+                res *= i; 
+            }
+
+            return res; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Console.WriteLine(factorialNumber(n)); // Print the factorial of n
+
+            Console.ReadLine(); // Wait for user input before exiting
+        }
+    }
+}
