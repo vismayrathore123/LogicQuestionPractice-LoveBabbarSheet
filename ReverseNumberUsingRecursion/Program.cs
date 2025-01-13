f05dbd84221980787913756dfdf506402a285c91
vismayrathore <vismayrathore2021@gmail.com>
2025-01-13 13:07:27 +0530

    added reverse a number using recusion

---
+++ ReverseNumberUsingRecursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversedNumber = ReverseNumber(number);
+
+        Console.WriteLine("Reversed number: " + reversedNumber);
+    }
+
+    // Recursive method to reverse the number
+    static int ReverseNumber(int num)
+    {
+        if (num == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return (num % 10) * (int)Math.Pow(10, (int)Math.Log10(num)) + ReverseNumber(num / 10);
+        }
+    }
+}
