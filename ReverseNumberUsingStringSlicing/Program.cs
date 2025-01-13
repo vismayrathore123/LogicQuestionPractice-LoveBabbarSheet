4a68b73d415460b809649f033e114f4a73ddf073
vismayrathore <vismayrathore2021@gmail.com>
2025-01-13 12:55:45 +0530

    added reverse number using string slicing

---
+++ ReverseNumberUsingStringSlicing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumberUsingStringSlicing
+{
+    public class Program
+    {
+        static int reverseNumber(int number)
+        {
+            string numString = number.ToString();
+            string reversedString = new string(numString.Reverse().ToArray());
+            return int.Parse(reversedString);
+        }
+        static void Main(string[] args)
+        {
+            int originalNumber = 12345;
+            int reversNumber = reverseNumber(originalNumber);
+            Console.WriteLine($"Reversed Number is :{ reversNumber}");
+
+
+        }
+    }
+}
