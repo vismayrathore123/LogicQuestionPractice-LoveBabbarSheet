0cf23dfaeebf61362a6bd3313c03380032a8876f
vismayrathore <vismayrathore2021@gmail.com>
2025-02-06 21:42:29 +0530

    abundant number, freindly pair

---
+++ AbundantNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbundantNumber
+{
+    public class Program
+    {
+        
+        static bool isAbundant(int number)
+        {
+            int sum = 0;
+            for (int i =0; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum > number;
+        }
+        static void Main(string[] args)
+        {
+            int number = 12;
+            if (isAbundant(number))
+            {
+                Console.WriteLine($"number is redundant{number}");
+            }
+            else
+            {
+                Console.WriteLine($"number is not abundant{number}");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ FreindlyPair/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FriendlyPair
+{
+    public class Program
+    {
+        static int SumOfDivisors(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        static bool IsFriendlyPair(int num1, int num2)
+        {
+            int sum1 = SumOfDivisors(num1);
+            int sum2 = SumOfDivisors(num2);
+
+            return (sum1 / num1 == sum2 / num2);
+        }
+
+        static void Main(string[] args)
+        {
+            int num1 = 6;
+            int num2 = 28;
+
+            if (IsFriendlyPair(num1, num2))
+            {
+                Console.WriteLine($"Yes, numbers {num1} and {num2} are friendly pairs.");
+            }
+            else
+            {
+                Console.WriteLine($"No, numbers {num1} and {num2} are not friendly pairs.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
